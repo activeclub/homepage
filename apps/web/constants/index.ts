e8b9346dcdc5cfeb269fb8822b47cc4c40850fb8
Naoto Nishizaka <nszk.nao@gmail.com>
2024-10-23 01:33:09 +0900

    Update social media links for YouTube and Xpaths and labels have been updated for YouTube and X

--- apps/web/constants/index.ts
+++ apps/web/constants/index.ts
@@ -2,6 +2,6 @@ import { Github, Twitter, Youtube } from "lucide-react";
 
 export const SOCIALS = [
   { label: "Github", path: "https://github.com/activeclub", icon: Github },
-  { label: "YouTube", path: "https://youtube.com/activeclub", icon: Youtube },
-  { label: "X", path: "https://x.com/activeclub", icon: Twitter },
+  { label: "YouTube", path: "https://www.youtube.com/@ActiveClub2024", icon: Youtube },
+  { label: "X", path: "https://x.com/active_club_", icon: Twitter },
 ];
