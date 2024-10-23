40284bf3e907e61b4ae9c9cd5a84277a1169020e
Naoto Nishizaka <nszk.nao@gmail.com>
2024-10-23 23:06:39 +0900

    Delete Next.js documentation files and update Node.js version requirement

---
+++ apps/web/app/(website)/opengraph-image.tsx
@@ -0,0 +1,37 @@
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+
+export const alt = "Active Club";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+export default async function Image() {
+  const logoSrc = await fetch(
+    new URL("../../public/logo.png", import.meta.url)
+  ).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "#000",
+          height: "100%",
+          width: "100%",
+        }}
+      >
+        <img src={logoSrc as never as string} height="140" />
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
