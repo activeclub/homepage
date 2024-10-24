606b897883ab8f4b11003f46e1e2f71df8c033e5
Naoto Nishizaka <nszk.nao@gmail.com>
2024-10-25 00:27:55 +0900

    Refactor discord token assertion logic

--- apps/web/src/lib/discord/env.ts
+++ apps/web/src/lib/discord/env.ts
@@ -1,6 +1,1 @@
-import { assertValue } from "../utils";
-
-export const token = assertValue(
-  process.env.DISCORD_TOKEN,
-  "Missing environment variable: DISCORD_TOKEN"
-);
+export const token = process.env.DISCORD_TOKEN as string;
