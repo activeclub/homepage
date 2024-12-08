531356ca07c741b118c0e1be96e3d1acddec63c9
Naoto Nishizaka <nszk.nao@gmail.com>
2024-12-08 23:13:12 +0900

    Add required validation for post fields

--- apps/web/src/lib/sanity/schema/post.ts
+++ apps/web/src/lib/sanity/schema/post.ts
@@ -9,6 +9,7 @@ export default defineType({
       name: "title",
       title: "Title",
       type: "string",
+      validation: (rule) => rule.required(),
     }),
     defineField({
       name: "slug",
@@ -18,12 +19,14 @@ export default defineType({
         source: "title",
         maxLength: 96,
       },
+      validation: (rule) => rule.required(),
     }),
     defineField({
       name: "author",
       title: "Author",
       type: "reference",
       to: { type: "author" },
+      validation: (rule) => rule.required(),
     }),
     defineField({
       name: "mainImage",
@@ -32,6 +35,7 @@ export default defineType({
       options: {
         hotspot: true,
       },
+      validation: (rule) => rule.required(),
     }),
     defineField({
       name: "categories",
@@ -43,6 +47,7 @@ export default defineType({
       name: "publishedAt",
       title: "Published at",
       type: "datetime",
+      validation: (rule) => rule.required(),
     }),
     defineField({
       name: "body",
